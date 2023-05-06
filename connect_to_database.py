3960da03cf6aa312049b614cd6bd80fed8803472
InColumi <max77648@gmail.com>
2023-05-06 11:34:06 +0300

    Refactor

--- connect_to_database.py
+++ connect_to_database.py
@@ -1,12 +1,9 @@
 from peewee import PostgresqlDatabase
-from config import Settings
+from config import settings
 
-setings = Settings()
 
-user = setings.user_name
-password = setings.password
-name_database = setings.name_database
-address = setings.address
-port = setings.port
-
-DB = PostgresqlDatabase(name_database, user=user, password=password, host=address, port=port)
+DB = PostgresqlDatabase(database=settings.name_database, 
+                        user=settings.user_name, 
+                        password=settings.password, 
+                        host=settings.address, 
+                        port=settings.port)
